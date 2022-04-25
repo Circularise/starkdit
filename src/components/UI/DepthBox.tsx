46c026bae1c9156292f200fbed6d522383580cd0
pouannes <pierreouannes@gmail.com>
2022-04-25 21:52:52 +0200

    feat: add variant to depth box

--- src/components/UI/DepthBox.tsx
+++ src/components/UI/DepthBox.tsx
@@ -2,12 +2,13 @@ import { Box, Flex } from "@chakra-ui/react";
 
 interface Props {
   depth: number | string;
+  variant?: "normal" | "small";
 }
-const DepthBox = ({ depth }: Props) => {
+const DepthBox = ({ depth, variant = "normal" }: Props) => {
   return (
     <Flex
       ml="auto"
-      p="0.5rem 1.125rem"
+      p={variant === "normal" ? "0.5rem 1.125rem" : "0.25rem 0.875rem"}
       bgColor="grey.grey3"
       borderRadius="2xl"
       fontWeight="bold"

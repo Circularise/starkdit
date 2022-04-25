9c03705fad25c3a03f4a5776f4569e4a4245df89
pouannes <pierreouannes@gmail.com>
2022-04-26 01:21:38 +0200

    feat: change base url of testnet

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,12 +3,20 @@ import NextHead from "next/head";
 import { InjectedConnector, StarknetProvider } from "@starknet-react/core";
 import { ChakraProvider } from "@chakra-ui/react";
 import theme from "~/utils/theme";
+import { Provider } from "starknet";
 
 function MyApp({ Component, pageProps }: AppProps) {
   const connectors = [new InjectedConnector()];
 
   return (
-    <StarknetProvider autoConnect connectors={connectors}>
+    <StarknetProvider
+      autoConnect
+      connectors={
+        new Provider({
+          baseUrl: "https://hackathon-4.starknet.io",
+        })
+      }
+    >
       <ChakraProvider theme={theme}>
         <NextHead>
           <title>StarkNet ❤️ React</title>
